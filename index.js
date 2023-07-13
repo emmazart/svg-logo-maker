7427f3fe62e95f449211575abcf6ac3913f1c87c
Emma Olson <emma.olson794@gmail.com>
2023-07-13 18:30:03 -0500

    user input being used to create new instances of each shape

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const {Triangle, Square, Circle} = require("./lib/shapes");
+
+let newShape;
 
 // declare questions array
 const questions = [
@@ -25,15 +28,41 @@ const questions = [
     {
         type: 'input',
         message: 'Enter a shape color (color keyword or a hexidecimal number):',
-        name: 'shapecolor',
+        name: 'fill',
         required: 'true' 
      }
 ];
 
+// function for writing new svg file
+function writeSvgFile({textColor, text, shape, fill}) {
+    // const {textColor, text, shape, fill} = newShape;
+    console.log(textColor, text, shape, fill);
+}
+
 // prompt user for input
 inquirer
     .prompt(questions)
-    .then(data => console.log(data));
+    .then(data => {
+        // console.log(data);
+        switch(data.shape){
+            case "square":
+                newShape = new Square(data.char, data.textcolor, data.shape, data.fill);
+                break;
+            case "triangle":
+                newShape = new Triangle(data.char, data.textcolor, data.shape, data.fill);
+                break;
+            case "circle":
+                newShape = new Circle(data.char, data.textcolor, data.shape, data.fill);
+                break;
+            default:
+                console.log("ERROR: You did not select a shape. Please try again.")
+                break;
+        }
+        console.log(newShape);
+    })
+    .then(() => {
+        writeSvgFile(newShape)
+    });
 // create svg file called logo.svg using inputs
     // logo.svg should be a 300x200 pixel image
 // print "Generated logo.svg" to command line
